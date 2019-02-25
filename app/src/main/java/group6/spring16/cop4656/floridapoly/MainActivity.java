1cb6378cc09e3d76de4d99954e5964e2fb58f3f3
Diegodlt <diegodltl@gmail.com>
2019-02-25 11:22:29 -0500

    Initial commit

---
+++ app/src/main/java/group6/spring16/cop4656/floridapoly/MainActivity.java
@@ -0,0 +1,13 @@
+package group6.spring16.cop4656.floridapoly;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
