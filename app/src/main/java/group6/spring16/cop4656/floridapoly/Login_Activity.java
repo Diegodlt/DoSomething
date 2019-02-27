eb7eedd9287d16da169c131642a4a51949b20d5f
Diegodlt <diegodltl@gmail.com>
2019-02-27 14:41:15 -0500

    Add login functionality to Login_Activity, allowing users to sign in

--- app/src/main/java/group6/spring16/cop4656/floridapoly/Login_Activity.java
+++ app/src/main/java/group6/spring16/cop4656/floridapoly/Login_Activity.java
@@ -25,23 +25,58 @@ import cz.msebera.android.httpclient.Header;
 
 public class Login_Activity extends AppCompatActivity {
 
-    Button loginButton;
+    private Button loginButton;
+    private TextView emailField;
+    private TextView passwordField;
+    private FirebaseAuth mAuth;
+
+    private String email;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
         loginButton = (Button)findViewById(R.id.loginButton);
 
         loginButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-//                Intent mapIntent = new Intent(Login_Activity.this, EventsActivity.class);
-//                startActivity(mapIntent);
+                selectTextFields();
+                getTextFromFields();
+                loginUser();
             }
         });
 
+    }
 
+    private void selectTextFields(){
+        emailField = (TextView)findViewById(R.id.email);
+        passwordField = (TextView)findViewById(R.id.password);
+    }
+
+    private void getTextFromFields(){
+        email = emailField.getText().toString().trim();
+        password = passwordField.getText().toString().trim();
+    }
+
+    private void loginUser(){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success
+                            Toast.makeText(Login_Activity.this, "Authentication success",
+                                    Toast.LENGTH_LONG).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(Login_Activity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
