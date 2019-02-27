d66df7039f72bc87172708f7ee130bf48212d716
Diegodlt <diegodltl@gmail.com>
2019-02-27 13:38:03 -0500

    Add create_user activity

---
+++ app/src/main/java/group6/spring16/cop4656/floridapoly/Create_Account.java
@@ -0,0 +1,13 @@
+package group6.spring16.cop4656.floridapoly;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Create_Account extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create__account_activity);
+    }
+}
